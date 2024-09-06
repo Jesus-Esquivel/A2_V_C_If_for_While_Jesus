ee8239956a7d036f59697e0c19ac7aa4bfcd693b
Jesus-Esquivel <a.22308051281176@cbtis128.edu.mx>
2024-09-06 10:22:00 -0600

    Add files via upload

---
+++ mifor.py
@@ -0,0 +1,3 @@
+nombre = ["Jesus", "Esquivel", "Martinez"] #esta asignan valores a la variable nombre
+for x in nombre: # Esta repitiendo la variable x
+  print(x) # imprime el valor x
---
+++ miwhile.py
@@ -0,0 +1,6 @@
+i = 0 # se le asigna un valor
+while i < 6: # esta comparando el valor asignado para que llegue hasta 6
+  i += 1 # lo que esta agregando una a la variable i
+  if i == 3: # esta comparando para descartar el numero deseado
+    continue # continua imprimiendo
+  print(i) # Esta imprimiendo los valores
