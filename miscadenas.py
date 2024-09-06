92801b922c11de4b38cdb60da27ed1abb345f944
Jesus-Esquivel <a.22308051281176@cbtis128.edu.mx>
2024-09-06 09:33:40 -0600

    Add files via upload
    Subi dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+a = """Jesus Adrian
+Trabajando con cadenas
+"""
+nombre="Jesus Adrian"
+print(nombre[0])
+print(nombre)
+misnovias=["Michelle","Andrea","Miranda"]
+for chicas in misnovias:
+ print(chicas)
+ 
+ print("otro ejemplo")
+ b = "Adrian Esquivel!"
+print(b[2:5])
+
+print(" a mayusculas")
+a = "Jesus Adrian!"
+print(a.upper())
+print("Concatenar")
+a = "Michelle "
+b = "Lozano"
+c = a+"_"+b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,18 @@
+import math
+edad=17   #17 se guarda en edad
+
+nombre="Jesus Adrian"
+print("tu edad es",edad)
+print("te llamas",nombre)
+print("--tipo de variables---")
+x = str(7)    # x will be '3'
+y = int(3)    # y will be 3
+z=float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas")
+x = math.sqrt(64)
+print(x)
+
+
